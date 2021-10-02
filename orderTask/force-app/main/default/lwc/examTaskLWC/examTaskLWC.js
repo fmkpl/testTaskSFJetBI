ed7fddf1c1ee3ffbe2741feaf41f1d7cd247a5e9
fmkpl <efimkopyltoppg@yandex.by>
2021-10-02 16:48:57 +0300

    added 2nd picklist for months

--- orderTask/force-app/main/default/lwc/examTaskLWC/examTaskLWC.js
+++ orderTask/force-app/main/default/lwc/examTaskLWC/examTaskLWC.js
@@ -1,30 +1,59 @@
 import { LightningElement, wire, track } from 'lwc';
 import getAllAccountNamesWithOrders from '@salesforce/apex/examTaskController.getAllAccountNamesWithOrders';
+import getAllMonthsOfPaymentDueDate from '@salesforce/apex/examTaskController.getAllMonthsOfPaymentDueDate';
 let i = 0;
 
 export default class ExamTaskLWC extends LightningElement {
-    value='';
-    @track error;
-    @track items = [];
+    accountName = '';
+    valueMonth = '';
+
+    @track errorNames;
+    @track accNames = [];
+
+    @track errorMonths;
+    @track monthNames = [];
 
     get accountNamesOptions() {
-        return this.items;
+        return this.accNames;
     }
 
     @wire(getAllAccountNamesWithOrders)
     wiredNames({error, data}) {
         if(data) {
+            this.accNames = [];
             for(i = 0; i < data.length; i++) {
-                this.items = [...this.items, {value: data[i], label: data[i]}]; 
+                this.accNames = [...this.accNames, {value: data[i], label: data[i]}]; 
             }
             error = undefined;
         } else if(error) {
-            this.error = error;
-            this.names = undefined;
+            this.errorNames = error;
+            this.accNames = undefined;
         }
     }
 
     handleChange(event) {
-        this.value = event.detail.value;
+        this.accountName = event.detail.value;
+    }
+
+    get monthNamesOptions() {
+        return this.monthNames;
+    }
+
+    @wire(getAllMonthsOfPaymentDueDate, { accountName: '$accountName'})
+    wiredMonths({error, data}) {
+        if(data) {
+            this.monthNames = [];
+            for(i = 0; i < data.length; i++) {
+                this.monthNames = [...this.monthNames, {value: data[i], label: data[i]}]; 
+            }
+            error = undefined;
+        } else if(error) {
+            this.errorMonths = error;
+            this.monthNames = undefined;
+        }
+    }
+
+    handleChangeMonth(event) {
+        this.valueMonth = event.detail.value;
     }
 }
