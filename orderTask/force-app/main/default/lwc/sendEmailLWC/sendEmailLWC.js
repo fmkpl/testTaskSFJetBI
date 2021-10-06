0579e14857ff6d88a6a624887d84a6111be7602a
fmkpl <efimkopyltoppg@yandex.by>
2021-10-06 12:42:02 +0300

    modified email lwc and added email method to controller class

--- orderTask/force-app/main/default/lwc/sendEmailLWC/sendEmailLWC.js
+++ orderTask/force-app/main/default/lwc/sendEmailLWC/sendEmailLWC.js
@@ -2,8 +2,22 @@ import { LightningElement, track, wire } from 'lwc';
 
 export default class SendEmailLWC extends LightningElement {
     @track email;
+    @track disabledFlag;
+    @track input;
 
     onChangeEmail(event) {
-        this.email = event.detail.value;
+        
+        this.input = this.template.querySelector(".emailInput");
+        if(this.input.value.length == 0) {
+            this.email = 'efimkopyltoppg@yandex.by';
+        }
+
+        if(!this.input.validity.valid) {
+            this.disabledFlag = true;
+            this.email = null;
+        } else if(this.input.validity.valid) {
+            this.disabledFlag = false;
+            this.email = event.detail.value;
+        }
     }
 }
