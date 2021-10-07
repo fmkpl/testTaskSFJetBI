ec4ee41640b73e47c33b2a41522f25ec77435447
fmkpl <efimkopyltoppg@yandex.by>
2021-10-07 20:55:06 +0300

    modified email send button, almost done

--- orderTask/force-app/main/default/lwc/sendEmailLWC/sendEmailLWC.js
+++ orderTask/force-app/main/default/lwc/sendEmailLWC/sendEmailLWC.js
@@ -1,43 +1,12 @@
-import { LightningElement, track, wire } from 'lwc';
-import getEmailOfCurrentUser from '@salesforce/apex/examTaskController.getEmailOfCurrentUser';
+import { LightningElement, track, api } from 'lwc';
 import sendEmailWithoutTemplate from "@salesforce/apex/examTaskController.sendEmailWithoutTemplate";
 
 export default class SendEmailLWC extends LightningElement {
-    @track email;
-    @track disabledFlag;
-    @track input;
     @track error;
-    staticEmail;
-
-    onChangeEmail(event) {
-        this.input = this.template.querySelector(".emailInput");
-
-        if(!this.input.validity.valid) {
-            this.disabledFlag = true;
-            this.email = null;
-        } else if(this.input.validity.valid) {
-            this.disabledFlag = false;
-            this.email = event.detail.value;
-        }
-    }
+    @api recordId;
 
     sendEmailFunc() {
-        if(this.email == '') {
-            this.email = this.staticEmail;
-        }
-
-        sendEmailWithoutTemplate({email: this.email});
-    }
-
-    @wire(getEmailOfCurrentUser)
-    wiredEmail({data, error}) {
-        if(data) {
-            this.email = data;
-            this.staticEmail = data;
-            error = undefined;
-        } else if(error) {
-            this.error = error;
-            this.email = undefined;
-        }
+        sendEmailWithoutTemplate({ recordId: this.recordId });
+        alert('Email sent.');
     }
 }
